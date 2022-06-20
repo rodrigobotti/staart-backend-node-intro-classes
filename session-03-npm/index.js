ee2ae313ab658f48665f906d4c5243da8860038c
Rodrigo Botti <rodrigo.botti@gmail.com>
2022-06-19 23:56:19 -0300

    Remove unused files

--- session-03-npm/index.js
+++
@@ -1,8 +0,0 @@
-const colors = require('./coloring')
-const { AttendingClass, ClassSucks, WelcomeToClass } = require('./messages')
-
-console.log('Aula 03 usando npm 😀 !')
-
-console.log(colors.happy(WelcomeToClass))
-console.log(colors.angry(ClassSucks))
-console.log(colors.info(AttendingClass))
